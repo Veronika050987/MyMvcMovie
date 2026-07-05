495310239e718e8a5a1a78c200e7b91312d90742
Veronika050987 <veronicaviv@mail.ru>
2026-07-05 14:16:41 +0300

    Add project files.

---
+++ MyMvcMovie/Data/MyMvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyMvcMovie.Models;
+
+namespace MyMvcMovie.Data
+{
+    public class MyMvcMovieContext : DbContext
+    {
+        public MyMvcMovieContext (DbContextOptions<MyMvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyMvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MyMvcMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMvcMovie.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Название фильма обязательно!")]
+		[StringLength(70)]
+		[Display(Name = "Movie title")]
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Строка содержит недопустимые символы")]
+		public string Title { get; set; }
+		[DataType(DataType.Date)]
+		[Display(Name = "Release date")]
+		public DateTime ReleaseDate { get; set; }
+		[RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$", ErrorMessage = "Строка содержит недопустимые символы")]
+		public string? Genre { get; set; }
+		public string? URL { get; set; }
+		public string? Brief { get; set; }
+		public string? Poster { get; set; }
+		public decimal Price { get; set; }
+	}
+}
